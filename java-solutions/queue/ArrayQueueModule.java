eafbba772943674f51ce1d39154a7f3dc3f9c66c
Ismaxis <Ismaxis@github.com>
2023-02-15 19:35:32 +0300

    HW-3 Module queue added

---
+++ java-solutions/queue/ArrayQueueModule.java
@@ -0,0 +1,80 @@
+package queue;
+
+
+import java.util.Arrays;
+import java.util.Objects;
+
+// Model:
+/*
+    enqueue - add to back
+    element - look at front
+    dequeue - remove from front
+    size
+    isEmpty - size == 0
+    clears
+ */
+public class ArrayQueueModule {
+    private static int size;
+    private static int head;
+    private static Object[] elements = new Object[2];
+
+    public static void enqueue(Object element) {
+        Objects.requireNonNull(element);
+        ensureCapacity(size + 1);
+
+        elements[(head + size) % elements.length] = element;
+        size++;
+    }
+    private static void ensureCapacity(int capacity) {
+        if (capacity > elements.length) {
+            Object[] newArr = Arrays.copyOf(elements, Math.max(elements.length * 2, capacity));
+            for (int i = 0; i < size; i++) {
+                newArr[i] = elements[(head + i) % elements.length];
+            }
+            elements = newArr;
+            head = 0;
+        }
+    }
+    public static Object element() {
+        assert size > 0;
+
+        return elements[head];
+    }
+
+    public static Object dequeue() {
+        assert size > 0;
+
+        Object result = elements[head];
+        size--;
+        head = (head + 1) % elements.length;
+        return result;
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public static void clear() {
+        elements = new Object[2];
+        size = 0;
+        head = 0;
+    }
+
+    public static void degPrint() {
+        System.out.println("--- dbg ---");
+        System.out.println("size: " +  size);
+        for (Object element : elements) {
+            System.out.print((element == null ? "n" : element) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < elements.length; i++) {
+            System.out.print(i == head ? "| " : "  ");
+        }
+        System.out.println("\n--- end ---\n");
+    }
+}
