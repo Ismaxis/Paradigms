5d94501f5c82a28b2fb8588cd00b1352e54aa6c4
Ismaxis <ismaxis@github.com>
2023-02-28 11:47:59 +0300

    * HW-3 Tests refactor

--- java-solutions/queue/tests/ArrayQueueTester.java
+++ java-solutions/queue/tests/ArrayQueueTester.java
@@ -3,6 +3,7 @@ package queue.tests;
 import java.util.Objects;
 
 abstract public class ArrayQueueTester {
+
     abstract public void test();
     protected static ContractFailure error(String methodName) {
         return new ContractFailure(methodName);
@@ -17,25 +18,27 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size() + 1;
         qf.enqueue(elements[0]);
 
-        compare(qf.size(), expectedSize,"enqueue(Object)", "n' = n + 1");
+        String methodName = "enqueue(Object)";
+        compare(qf.size(), expectedSize, methodName, "n' = n + 1");
         qf.clear();
 
         fillQueue(qf, elements);
         qf.enqueue("Test last element, that shouldn't be dequeued");
 
-        checkImmutable(elements, qf, "enqueue(Object)", "immutable(n)");
+        checkImmutable(elements, qf, methodName, "immutable(n)");
     }
     public void testClear(QueueFunctions qf) {
         for (int i = 0; i < 5; i++) {
             qf.enqueue(2 * i);
         }
         qf.clear();
-        compare(qf.size(), 0, "clear()", "n' = 0");
+        String methodName = "clear()";
+        compare(qf.size(), 0, methodName, "n' = 0");
 
         Integer testElem = 88;
         qf.enqueue(testElem);
         Object head = qf.dequeue();
-        compare(head, testElem, "clear()","Queue contains elements after clear");
+        compare(head, testElem, methodName,"Queue contains elements after clear");
     }
     public void testDequeue(QueueFunctions qf) {
         Object[] elements = {"acabaca", 2.7, 100, "___"};
@@ -44,7 +47,8 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size() - 1;
         qf.dequeue();
 
-        compare(qf.size(), expectedSize,"dequeue()", "n' = n - 1");
+        String methodName = "dequeue()";
+        compare(qf.size(), expectedSize, methodName, "n' = n - 1");
 
         String head = "First element to be dequeued";
         String head2 = "Second element to be dequeued";
@@ -52,10 +56,10 @@ abstract public class ArrayQueueTester {
         qf.enqueue(head2);
         fillQueue(qf, elements);
 
-        compare(qf.dequeue(), head, "dequeue()", "R = a[1]");
-        compare(qf.dequeue(), head2, "dequeue()", "a'[1] = a[2]");
+        compare(qf.dequeue(), head, methodName, "R = a[1]");
+        compare(qf.dequeue(), head2, methodName, "a'[1] = a[2]");
 
-        checkImmutable(elements, qf, "dequeue()", "for i in [1; n']: a'[i] = a[i + 1]");
+        checkImmutable(elements, qf, methodName, "for i in [1; n']: a'[i] = a[i + 1]");
     }
     public void testElement(QueueFunctions qf) {
         Object[] elements = {3.14, "^_^", 200, "(#_#)"};
@@ -64,27 +68,29 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size();
         qf.element();
 
-        compare(qf.size(), expectedSize,"element()", "n' = n");
+        String methodName = "element()";
+        compare(qf.size(), expectedSize, methodName, "n' = n");
         qf.clear();
 
         String head = "Element to be dequeued";
         qf.enqueue(head);
         fillQueue(qf, elements);
 
-        compare(qf.element(), head, "element()", "R = a[1]");
+        compare(qf.element(), head, methodName, "R = a[1]");
         qf.dequeue();
 
-        checkImmutable(elements, qf, "element()", "immutable(n)");
+        checkImmutable(elements, qf, methodName, "immutable(n)");
     }
     public void testSize(QueueFunctions qf) {
-        compare(qf.size(), 0, "size()", "Initial size should be 0");
+        String methodName = "size()";
+        compare(qf.size(), 0, methodName, "Initial size should be 0");
         Object[] elements = {"/_/", "--_--", 1020, 0};
 
         qf.enqueue(elements[0]);
         int expectedSize = qf.size();
         qf.size();
 
-        compare(qf.size(), expectedSize,"size()", "n' = n");
+        compare(qf.size(), expectedSize, methodName, "n' = n");
         qf.clear();
 
         fillQueue(qf, elements);
@@ -92,8 +98,8 @@ abstract public class ArrayQueueTester {
         expectedSize = qf.size();
         // Immutable check
         for (Object element : elements) {
-            compare(qf.dequeue(), element, "size()","immutable(n)");
-            compare(qf.size(), --expectedSize, "size()","R = n");
+            compare(qf.dequeue(), element, methodName,"immutable(n)");
+            compare(qf.size(), --expectedSize, methodName,"R = n");
         }
     }
     public void testEmpty(QueueFunctions qf) {
@@ -104,7 +110,8 @@ abstract public class ArrayQueueTester {
         qf.isEmpty();
 
         // Size check
-        compare(qf.size(), expectedSize,"isEmpty()", "n' = n");
+        String methodName = "isEmpty()";
+        compare(qf.size(), expectedSize, methodName, "n' = n");
         qf.clear();
 
         fillQueue(qf, elements);
@@ -112,8 +119,8 @@ abstract public class ArrayQueueTester {
 
         // Immutable check
         for (Object element : elements) {
-            compare(qf.dequeue(), element, "isEmpty()","immutable(n)");
-            compare(qf.isEmpty(), qf.size() == 0, "isEmpty()","R = (n == 0)");
+            compare(qf.dequeue(), element, methodName,"immutable(n)");
+            compare(qf.isEmpty(), qf.size() == 0, methodName,"R = (n == 0)");
         }
     }
     public void testPush(QueueFunctions qf) {
@@ -124,13 +131,14 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size() + 1;
         qf.push(elements[0]);
 
-        compare(qf.size(), expectedSize,"push(Object)", "n' = n + 1");
+        String methodName = "push(Object)";
+        compare(qf.size(), expectedSize, methodName, "n' = n + 1");
         qf.clear();
 
         fillQueue(qf, elements);
         qf.push(elementsWithFirst[0]);
 
-        checkImmutable(elementsWithFirst, qf, "push(Object)", "a[1] = element && for i in [1; n]: a'[i + 1] = a[i]");
+        checkImmutable(elementsWithFirst, qf, methodName, "a[1] = element && for i in [1; n]: a'[i + 1] = a[i]");
     }
     public void testRemove(QueueFunctions qf) {
         Object[] elements = {"abba", 36.6, 12301823};
@@ -141,13 +149,14 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size() - 1;
         qf.remove();
 
-        compare(qf.size(), expectedSize,"remove()", "n' = n - 1");
+        String methodName = "remove()";
+        compare(qf.size(), expectedSize, methodName, "n' = n - 1");
 
         fillQueue(qf, elementsWithLast);
 
-        compare(qf.remove(), elementsWithLast[elementsWithLast.length - 1], "remove()", "R = a[n]");
+        compare(qf.remove(), elementsWithLast[elementsWithLast.length - 1], methodName, "R = a[n]");
 
-        checkImmutable(elements, qf, "remove()", "immutable(n - 1)]");
+        checkImmutable(elements, qf, methodName, "immutable(n - 1)]");
     }
     public void testPeek(QueueFunctions qf) {
         Object[] elements = {3.14, "^_^", 200, "(#_#)"};
@@ -156,17 +165,18 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size();
         qf.element();
 
-        compare(qf.size(), expectedSize,"peek()", "n' = n");
+        String methodName = "peek()";
+        compare(qf.size(), expectedSize, methodName, "n' = n");
         qf.clear();
 
         fillQueue(qf, elements);
         String tail = "Element to be dequeued";
         qf.enqueue(tail);
 
-        compare(qf.peek(), tail, "peek()", "R = a[n]");
+        compare(qf.peek(), tail, methodName, "R = a[n]");
         qf.remove();
 
-        checkImmutable(elements, qf, "peek()", "immutable(n)");
+        checkImmutable(elements, qf, methodName, "immutable(n)");
     }
     public void testToArray(QueueFunctions qf) {
         Object[] elements = {"abba", 36.6, 12301823, "-_-", 202, "(@_@)", 1000};
@@ -175,17 +185,18 @@ abstract public class ArrayQueueTester {
         int expectedSize = qf.size();
         Object[] elementsFromToArray = qf.toArray();
 
-        compare(qf.size(), expectedSize, "toArray()", "n' = n");
+        String methodName = "toArray()";
+        compare(qf.size(), expectedSize, methodName, "n' = n");
 
         if (elements.length != elementsFromToArray.length) {
-            throw error("toArray()",
+            throw error(methodName,
                     "Wrong length. Expected: " + elements.length + ", found: " + elementsFromToArray.length);
         }
         for (int i = 0; i < elements.length; i++) {
-            compare(elementsFromToArray[i], elements[i], "toArray()", "b[i] = a[i]");
+            compare(elementsFromToArray[i], elements[i], methodName, "b[i] = a[i]");
         }
 
-        checkImmutable(elements, qf, "toArray()", "immutable(n)");
+        checkImmutable(elements, qf, methodName, "immutable(n)");
     }
     private static void fillQueue(QueueFunctions qf, Object[] elements) {
         for (Object element : elements) {
