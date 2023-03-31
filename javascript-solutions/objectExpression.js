7f34ba253f43f01731596e509474d98163f94678
Ismaxis <Ismaxis@github.com>
2023-03-31 20:17:26 +0300

    * HW-8 Cleanup

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -1,6 +1,7 @@
 function Const(value) {
     this.value = value;
 }
+
 Const.prototype.evaluate = function() {
     return this.value;
 };
@@ -9,9 +10,7 @@ Const.prototype.toString = function() {
 };
 Const.prototype.prefix = Const.prototype.toString;
 Const.prototype.postfix = Const.prototype.toString;
-Const.prototype.diff = function() {
-    return ZERO;
-};
+Const.prototype.diff = () => ZERO;
 Const.prototype.simplify = function() {
     return this;
 };
@@ -21,20 +20,19 @@ const ZERO = new Const(0);
 const ONE = new Const(1);
 const TWO = new Const(2);
 
-function isConstantValue(element, value) {
-    return element instanceof Const && element.value === value;
-}
+const isConstantValue = (element, value) => element instanceof Const && element.value === value;
 const isZero = element => isConstantValue(element, 0);
-function isOne(element) {
-    return isConstantValue(element, 1);
-}
+const isOne = element => isConstantValue(element, 1);
 
-const variableSymbolsToIndex = { 'x': 0, 'y': 1, 'z': 2 };
+const variableSymbolsToIndex = {'x': 0, 'y': 1, 'z': 2};
+
 function Variable(symbol) {
     this.symbol = symbol;
+    this.index = variableSymbolsToIndex[symbol];
 }
+
 Variable.prototype.evaluate = function(...values) {
-    return values[variableSymbolsToIndex[this.symbol]];
+    return values[this.index];
 };
 Variable.prototype.toString = function() {
     return this.symbol.toString();
@@ -52,6 +50,7 @@ function AbstractOperation(...operands) {
     this.operands = operands;
     this.evaluateOperands = operands;
 }
+
 AbstractOperation.prototype.evaluate = function(...values) {
     return this.operation(...this.evaluateOperands.map(x => x.evaluate(...values)));
 };
@@ -65,13 +64,11 @@ AbstractOperation.prototype.postfix = function() {
     return '(' + this.operands.map(op => op.postfix()).join(' ') + ' ' + this.symbol + ')';
 };
 AbstractOperation.prototype.diff = function(varName) {
-    const operands = this.operands;
     if (this.diffCoefficients === undefined) {
         this.diffCoefficients = this.getDiffCoefficients(...this.operands);
     }
-    const coefficients = this.diffCoefficients;
-    return operands.reduce((sum, curOperand, i) =>
-        new Add(new Multiply(coefficients[i], curOperand.diff(varName)), sum), ZERO);
+    return this.operands.reduce((sum, curOperand, i) =>
+        new Add(new Multiply(this.diffCoefficients[i], curOperand.diff(varName)), sum), ZERO);
 };
 AbstractOperation.prototype.simplify = function() {
     const operandsSimplified = this.operands.map(operand => operand.simplify());
@@ -88,12 +85,11 @@ AbstractOperation.prototype.simplify = function() {
 };
 
 function createOperation(symbol, operation, diffCoefficients, simplifySpecificRules) {
-    function Operation (...operands) {
+    function Operation(...operands) {
         AbstractOperation.call(this, ...operands);
     }
-    Operation.getArgsCount = function() {
-        return operation.length;
-    };
+
+    Operation.getArgsCount = () => operation.length;
     Operation.prototype = Object.create(AbstractOperation.prototype);
     Operation.prototype.constructor = Operation;
     Operation.prototype.symbol = symbol;
@@ -108,106 +104,87 @@ const Add = createOperation('+',
     () => [ONE, ONE],
     (leftSimplified, rightSimplified) =>
         isZero(leftSimplified) ? rightSimplified :
-        isZero(rightSimplified) ? leftSimplified :
-        new Add(leftSimplified, rightSimplified)
+            isZero(rightSimplified) ? leftSimplified :
+                new Add(leftSimplified, rightSimplified)
 );
 
 const Subtract = createOperation('-',
     (a, b) => a - b,
     () => [ONE, MINUS_ONE],
-    (leftSimplified, rightSimplified) => {
-        if (isZero(leftSimplified)) {
-            return new Negate(rightSimplified).simplify();
-        } else if (isZero(rightSimplified)) {
-            return leftSimplified;
-        } else {
-            return new Subtract(leftSimplified, rightSimplified);
-        }
-    }
+    (leftSimplified, rightSimplified) =>
+        isZero(leftSimplified) ? new Negate(rightSimplified).simplify() :
+            isZero(rightSimplified) ? leftSimplified :
+                new Subtract(leftSimplified, rightSimplified)
 );
 
 const Multiply = createOperation('*',
     (a, b) => a * b,
     (left, right) => [right, left],
-    (leftSimplified, rightSimplified) => {
-        if (isZero(leftSimplified) || isZero(rightSimplified)) {
-            return ZERO;
-        } else if (isOne(leftSimplified)) {
-            return rightSimplified;
-        } else if (isOne(rightSimplified)) {
-            return leftSimplified;
-        } else {
-            return new Multiply(leftSimplified, rightSimplified);
-        }
-    }
+    (leftSimplified, rightSimplified) =>
+        isZero(leftSimplified) || isZero(rightSimplified) ? ZERO :
+            isOne(leftSimplified) ? rightSimplified :
+                isOne(rightSimplified) ? leftSimplified :
+                    new Multiply(leftSimplified, rightSimplified)
 );
 
 const Divide = createOperation('/',
     (a, b) => a / b,
     (left, right) => [new Divide(ONE, right), new Divide(new Negate(left), new Multiply(right, right))],
-    (leftSimplified, rightSimplified) => {
-        if (isZero(leftSimplified)) {
-            return ZERO;
-        } else if (isOne(rightSimplified)) {
-            return leftSimplified;
-        } else {
-            return new Divide(leftSimplified, rightSimplified);
-        }
-    }
+    (leftSimplified, rightSimplified) =>
+        isZero(leftSimplified) ? ZERO :
+            isOne(rightSimplified) ? leftSimplified :
+                new Divide(leftSimplified, rightSimplified)
 );
 
 function AbstractComplexSumOperation(evaluateOperandsFunc, ...operands) {
     AbstractOperation.call(this, ...operands);
     this.evaluateOperands = evaluateOperandsFunc(operands);
 }
+
 AbstractComplexSumOperation.prototype = Object.create(AbstractOperation.prototype);
 
-const createComplexSumOperation = function(argsCount, symbol, evalOperands, operation, diffCoefficients, simplifySpecificRules) {
-    function ComplexOperation (...operands) {
+function createComplexSumOperation(argsCount, symbol, evalOperands, operation, diffCoefficients, simplifySpecificRules) {
+    function ComplexOperation(...operands) {
         AbstractComplexSumOperation.call(this, evalOperands, ...operands);
     }
-    ComplexOperation.getArgsCount = function() {
-        return argsCount;
-    };
+
+    ComplexOperation.getArgsCount = () => argsCount;
     ComplexOperation.prototype = Object.create(
         createOperation(symbol, operation, diffCoefficients, simplifySpecificRules).prototype);
-    ComplexOperation.prototype.sum = (...operandValues) => operandValues.reduce((sum, curVal) => sum + curVal);
+    ComplexOperation.prototype.sum = operandValues => operandValues.reduce((sum, curVal) => sum + curVal, 0);
     ComplexOperation.prototype.constructor = ComplexOperation;
     return ComplexOperation;
-};
-
-function createSumSqN(argsCount) {
-    return createComplexSumOperation(argsCount, 'sumsq' + argsCount,
-        operands => operands.map(x => new Square(x)),
-        function(...operandValues) {
-            return this.sum(...operandValues);
-        },
-        (...operands) => operands.map(x => new Multiply(TWO, x)))
 }
 
-// :NOTE: const [Sumsq2, Sumsq3] = [2, 3].map(createSumSqN)
+const createSumSqN = argsCount => createComplexSumOperation(argsCount, 'sumsq' + argsCount,
+    operands => operands.map(x => new Square(x)),
+    function(...operandValues) {
+        return this.sum(operandValues);
+    },
+    (...operands) => operands.map(x => new Multiply(TWO, x))
+);
+
 const SumsqN = [2, 3, 4, 5].map(createSumSqN);
 const [Sumsq2, Sumsq3, Sumsq4, Sumsq5] = SumsqN;
-const createDistanceN = function(argsCount) {
-    return createComplexSumOperation(argsCount, 'distance' + argsCount.toString(),
-        // :NOTE: new type
-        operands => [new (SumsqN[operands.length - 2])(...operands)],
-        function(...operandValues) {
-            return Math.sqrt(this.sum(...operandValues));
-        },
-        function (...operands) {
-            return operands.map(x => new Divide(x, this));
-        }
-    );
-};
+
+const createDistanceN = argsCount => createComplexSumOperation(argsCount, 'distance' + argsCount,
+    operands => [new (SumsqN[operands.length - 2])(...operands)],
+    function(...operandValues) {
+        return Math.sqrt(this.sum(operandValues));
+    },
+    function(...operands) {
+        return operands.map(x => new Divide(x, this));
+    }
+);
+
 const [Distance2, Distance3, Distance4, Distance5] = [2, 3, 4, 5].map(createDistanceN);
 
 const Sumexp = createComplexSumOperation(Infinity, 'sumexp',
     operands => operands.map(x => new Exp(x)),
     function(...operandValues) {
-        return  operandValues.length > 0 ? this.sum(...operandValues) : 0;
+        return this.sum(operandValues);
     },
-    function () {
+    function() {
         return this.evaluateOperands;
     }
 );
@@ -215,9 +192,9 @@ const Sumexp = createComplexSumOperation(Infinity, 'sumexp',
 const LSE = createComplexSumOperation(Infinity, 'lse',
     operands => [new Sumexp(...operands)],
     function(...operandValues) {
-            return operandValues.length > 0 ? Math.log(this.sum(...operandValues)) : 0;
-        },
-    function (...operands) {
+        return Math.log(this.sum(operandValues));
+    },
+    function(...operands) {
         return operands.map(x => new Divide(new Exp(x), this.evaluateOperands[0]));
     }
 );
@@ -239,28 +216,31 @@ const Negate = createOperation('negate',
     () => [MINUS_ONE]
 );
 
-const literals = { 'x': new Variable('x'), 'y': new Variable('y'), 'z': new Variable('z'), };
-const operations = { '+': Add, '-': Subtract, '*': Multiply, '/': Divide, "negate": Negate,
-                    "sumsq2": Sumsq2, "sumsq3": Sumsq3, "sumsq4": Sumsq4, "sumsq5": Sumsq5,
-                    "distance2": Distance2, "distance3": Distance3, "distance4": Distance4, "distance5": Distance5,
-                    "sumexp": Sumexp, "lse": LSE, };
+const literals = {'x': new Variable('x'), 'y': new Variable('y'), 'z': new Variable('z'),};
+const operations = {
+    '+': Add, '-': Subtract, '*': Multiply, '/': Divide, "negate": Negate,
+    "sumsq2": Sumsq2, "sumsq3": Sumsq3, "sumsq4": Sumsq4, "sumsq5": Sumsq5,
+    "distance2": Distance2, "distance3": Distance3, "distance4": Distance4, "distance5": Distance5,
+    "sumexp": Sumexp, "lse": LSE,
+};
 
-// :NOTE: split :FIXED:
 const parse = str => str.trim().split(/\s+/).reduce((stack, token) => {
-        if (isConst(token)) {
-            stack.push(new Const(parseInt(token)));
-        } else if (token in literals) {
-            stack.push(literals[token]);
-        } else if (token in operations) {
-            const op = operations[token];
-            stack.push(new op(...stack.splice(-op.getArgsCount())));
-        }
-        return stack
-    }, []).pop();
+    if (isConst(token)) {
+        stack.push(new Const(parseInt(token)));
+    } else if (token in literals) {
+        stack.push(literals[token]);
+    } else if (token in operations) {
+        const op = operations[token];
+        stack.push(new op(...stack.splice(-op.getArgsCount())));
+    }
+    return stack
+}, []).pop();
+const isConst = str => isFinite(str);
 
 function ParserException(index, message) {
     this.message = index + ": " + message;
 }
+
 ParserException.prototype = Object.create(Error.prototype);
 ParserException.prototype.name = "ParserException";
 ParserException.prototype.constructor = ParserException;
@@ -269,6 +249,7 @@ function createParserException(name, messageGenerator) {
     function CustomParserException(index, ...args) {
         ParserException.call(this, index, messageGenerator(...args));
     }
+
     CustomParserException.prototype = Object.create(ParserException.prototype);
     CustomParserException.prototype.name = name;
     CustomParserException.prototype.constructor = CustomParserException;
@@ -287,7 +268,7 @@ const OperationAsArgumentException = createParserException(
 
 const PrematureEndException = createParserException(
     "PrematureEndException",
-        expected => "Premature end." + (expected ? (" '" + expected + "' expected") : ""));
+    expected => "Premature end." + (expected ? (" '" + expected + "' expected") : ""));
 
 const WrongArgumentsCountException = createParserException(
     "WrongArgumentsCountException",
@@ -298,24 +279,26 @@ const IndexOutOfBoundException = createParserException(
     "IndexOutOfBoundException",
     (index, str) => "Index '" + index + "' out of bounds for string '" + str + "'");
 
-const skipWhiteSpaces = source => {
-    while(source.index < source.str.length && /\s/.test(source.str.charAt(source.index))) {
+function skipWhiteSpaces(source) {
+    while (source.index < source.str.length && /\s/.test(source.str.charAt(source.index))) {
         source.index++;
     }
-};
-const parseToken = source => {
+}
+
+function parseToken(source) {
     skipWhiteSpaces(source);
     if (source.str.charAt(source.index) === '(') {
         return '(';
     }
     const startIndex = source.index;
     let curChar = source.str.charAt(source.index);
-    while(source.index < source.str.length && curChar !== ')' && curChar !== '(' && !/\s/.test(curChar)) {
+    while (source.index < source.str.length && curChar !== ')' && curChar !== '(' && !/\s/.test(curChar)) {
         source.index++;
         curChar = source.str.charAt(source.index);
     }
     return source.str.substring(startIndex, source.index);
-};
+}
+
 function parsePrimitive(index, token) {
     if (isConst(token)) {
         return new Const(parseInt(token));
@@ -347,7 +330,7 @@ function parseOperand(source, takeOperation) {
                     throw new OperationAsArgumentException(source.index, token);
                 }
             } else {
-                parsedTokens.push(parsePrimitive(source.index, token))
+                parsedTokens.push(parsePrimitive(source.index, token));
             }
             skipWhiteSpaces(source);
         }
@@ -369,35 +352,16 @@ function parseOperand(source, takeOperation) {
         return parsePrimitive(source.index, firstToken);
     }
 }
-const parsePrefix = str => {
-    const source = { str: str, index: 0 };
-    const res = parseOperand(source, parsedTokens => parsedTokens.shift());
-    // :NOTE: copy-paste
+
+function parseString(str, takeOperation) {
+    const source = {str: str, index: 0};
+    const res = parseOperand(source, takeOperation);
     skipWhiteSpaces(source);
     if (source.index < source.str.length) {
         throw new PrematureEndException(source.index);
     }
     return res;
-};
+}
 
-const parsePostfix = str => {
-    const source = { str: str, index: 0 };
-    const res = parseOperand(source, parsedTokens => parsedTokens.pop());
-    skipWhiteSpaces(source);
-    if (source.index < source.str.length) {
-        throw new PrematureEndException(source.index);
-    }
-    return res;
-};
-
-// :NOTE: 1.1e-1 :FIXED:
-const isConst = str => isFinite(str);
-
-const exp = new Sumsq2(new Const(2), new Const(3));
-console.log(exp.toString());
-console.log(exp.evaluate(0,0,0));
-const diffExp = exp.diff('x');
-console.log(diffExp.toString());
-const simpExp = exp.simplify();
-console.log(simpExp.toString());
-
+const parsePrefix = str => parseString(str, parsedTokens => parsedTokens.shift());
+const parsePostfix = str => parseString(str, parsedTokens => parsedTokens.pop());
