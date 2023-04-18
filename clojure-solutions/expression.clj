88777b573f4530c7204fb35d7ea6445c8138c156
Ismaxis <ismaxis@github.com>
2023-04-18 12:07:23 +0300

    + HW-10 SumEx-Lse Mod

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -1,3 +1,4 @@
+(require 'clojure.math)
 (defn constant [val] (fn [& _] val))
 (defn variable [name] (fn [map] (get map name)))
 (defn operation [op] (fn [& args] (fn [map] (apply op (mapv #(% map) args)))))
@@ -7,8 +8,12 @@
 (def divide (operation (fn [& args]
                 (if (== (count args) 1) (/ 1 (double (first args)))
                                         (reduce #(/ %1 (double %2)) args)))))
+
+(defn sum-of-exp [& args] (apply + (mapv clojure.math/exp args)))
+(def sumexp (operation sum-of-exp))
+(def lse (operation (fn [& args] (clojure.math/log (apply sum-of-exp args)))))
 (def negate (operation -'))
-(def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate})
+(def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate, 'sumexp sumexp, 'lse lse})
 (defn parseToken [token] (cond
     (number? token) (constant token)
     (symbol? token) (variable (str token))
@@ -20,8 +25,7 @@
 
 ;(def expr-str "(- (* 2 x) 3)")
 ;(def print-t (comp println type))
-;;(def parsed (parseFunction expr-str))
-;(def parsed (divide (constant 5) (constant -0)))
+;;;(def parsed (parseFunction expr-str))
+;(def parsed (sumexp (variable "x")))
 ;(print-t parsed)
-;(println (parsed {"z" 0.0, "x" 0.0, "y" 0.0}))
-;(println)
+;(println (parsed {"z" 0.0, "x" 1.0, "y" 0.0}))
