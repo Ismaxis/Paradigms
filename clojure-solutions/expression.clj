e37912e7f118635bd07e079488cd3e1dc3fa04b6
Ismaxis <Ismaxis@github.com>
2023-04-24 23:30:29 +0300

    * HW-11 Common parser extracted

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -4,6 +4,17 @@
   ([] 1)
   ([arg] (/ 1 (double arg)))
   ([first & args] (/ first (double (apply * args)))))
+(defn parser [const-builder var-builder op-map]
+  (letfn [(build-tree [token]
+    (cond
+      (number? token) (const-builder token)
+      (symbol? token) (var-builder (str token))
+      (list? token) (apply (get op-map (first token))
+                           (mapv build-tree (rest token)))
+    ))](comp build-tree read-string)
+  ))
+
+; =============== HW-10 ===============
 
 (def constant constantly)
 (defn variable [name] (fn [map] (get map name)))
@@ -16,13 +27,8 @@
 (def sumexp (operation sum-of-exp))
 (def lse (operation (fn [& args] (clojure.math/log (apply sum-of-exp args)))))
 (def negate (operation -'))
-(def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate, 'sumexp sumexp, 'lse lse})
-(defn parseToken [token] (cond
-    (number? token) (constant token)
-    (symbol? token) (variable (str token))
-    (list? token) (apply (get operations (first token)) (mapv parseToken (rest token)))
-    ))
-(defn parseFunction [str] ((comp parseToken read-string) str))
+(def operations-func { '+ add, '- subtract, '* multiply, '/ divide, 'negate negate, 'sumexp sumexp, 'lse lse })
+(def parseFunction (parser constant variable operations-func))
 
 ; =============== HW-11 ===============
 
@@ -43,10 +49,10 @@
 (def MINUS_ONE (Constant -1))
 (deftype VariableClass [name]
   Object
-  (toString [this] (.-name this))
+  (toString [this] (str (.-name this)))
   Expression
-  (eval [this map] (get map (.-name this)))
-  (diff [this varName] (if (= (.-name this) varName) ONE ZERO))
+  (eval [this map] (get map (.toString this)))
+  (diff [this varName] (if (= (.toString this) varName) ONE ZERO))
   )
 (defn Variable [name] (VariableClass. name))
 (declare Add)
@@ -61,48 +67,35 @@
 (defn Negate [& operands] (OperationClass. 'negate - operands (fn [& _] MINUS_ONE)))
 (defn Add [& operands] (OperationClass. '+ + operands (fn [& _] ONE)))
 (defn Subtract [& operands] (OperationClass. '- - operands
-                                             (fn [_ i & operands]
-                                               (if (zero? i) (if (== (count operands) 1) MINUS_ONE ONE) MINUS_ONE))))
-(defn Multiply [& operands]
-  (OperationClass. '* * operands
+   (fn [_ i & operands]
+     (if (zero? i) (if (== (count operands) 1) MINUS_ONE ONE) MINUS_ONE))))
+(defn Multiply [& operands] (OperationClass. '* * operands
    (fn [_ i & operands] (apply Multiply (keep-indexed #(when ((comp not =) i %1) %2) operands)))))
-(defn Divide [& operands]
-  (OperationClass. '/ division operands
-   (fn [this i numerator & denominators]
-     (if (zero? (count denominators))
-       (Negate (Divide this numerator))
-      (if (== i 0)
+(defn Divide [& operands] (OperationClass. '/ division operands
+ (fn [this i numerator & denominators]
+   (if (zero? (count denominators))
+    (Negate (Divide this numerator))
+    (if (== i 0)
       (apply Divide ONE denominators)
       (Negate (Divide this (nth denominators (- i 1))))
-      ))
-    )))
-
+    ))
+ )))
+(def operations-obj { '+ Add, '- Subtract, '* Multiply, '/ Divide 'negate Negate }) ;, 'sumexp sumexp, 'lse lse })
+(def parseObject (parser Constant Variable operations-obj))
 (defn evaluate [expression vars] (.eval expression vars))
 (defn toString [expression] (.toString expression))
 (defn diff [expression varName] (.diff expression varName))
 
-;; TODO common parser
-(def operationsObj { '+ Add, '- Subtract, '* Multiply, '/ Divide 'negate Negate }) ;, 'sumexp sumexp, 'lse lse})
-(defn parseTokenObj [token] (cond
-    (number? token) (Constant token)
-     (symbol? token) (Variable (str token))
-     (list? token) (apply (get operationsObj (first token)) (mapv parseTokenObj (rest token)))
-    ))
-
-(defn parseObject [str] ((comp parseTokenObj read-string) str))
-
 ; =============== TEST ZONE ===============
-(def expr (diff (Divide (Variable "x")) "x"))
-;(def expr (parseObject "(- (* 2 x) 3)"))
 
-(println "abacaba")
-(println (type expr))
-(println (toString expr))
-(println (evaluate expr {"z" 1.0, "x" 1.0, "y" 1.0}))
+;(def expr (diff (Divide (Variable "x")) "x"))
+;(def expr (parseObject "(- (* 2 x) 1)"))
+;(println (toString expr))
+;(println (evaluate expr {"z" 1.0, "x" 1.0, "y" 1.0}))
 ;(def diffed (diff expr "x"))
 ;(println diffed)
 ;(println (toString diffed))
-;(println (evaluate diffed {"z" 2}))
+;(println (evaluate diffed {"x" 2}))
 
 ;(def expr-str "(+ 1 (* x 2))")
 ;(def print-t (comp println type))
