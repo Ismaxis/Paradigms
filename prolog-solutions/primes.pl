e13fba6c6a787c7c811c51690fdeb3c80ad0e318
Ismaxis <Ismaxis@github.com>
2023-05-18 00:31:08 +0300

    + HW-13 Hard primes

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -22,3 +22,25 @@ prime(N) :- min_divisor(N, D), D = N.
 prime(N) :- \+ min_divisor(N, _).
 
 composite(N) :- \+ prime(N).
+
+sorted_primes(_, []).
+sorted_primes(MAX_IN_HEAD, [H]) :- MAX_IN_HEAD =< H, prime(H).
+sorted_primes(MAX_IN_HEAD, [H1, H2 | T]) :-
+    MAX_IN_HEAD =< H1,
+    H1 =< H2,
+    prime(H1),
+    prime(H2),
+    sorted_primes(H2, T).
+
+prime_divisors(1, []) :- !.
+prime_divisors(N, [N]) :- number(N), prime(N), !.
+prime_divisors(N, [H | T]) :-
+    number(N),
+    min_divisor(N, H),
+    N1 is N // H,
+    prime_divisors(N1, T).
+prime_divisors(N, [H | T]) :-
+    \+ number(N), number(H),
+    sorted_primes(0, [H | T]),
+    prime_divisors(N1, T),
+    N is N1 * H.
